c0a64a80a672b29095ae92231072ce6d3432273c
Sascha Obst <saschaobst@Saschas-MacBook-Pro.local>
2019-11-14 23:11:15 +0100

    final changes

--- ddm-lmp/src/main/java/de/hpi/ddm/MasterSystem.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/MasterSystem.java
@@ -1,14 +1,12 @@
 package de.hpi.ddm;
 
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.cluster.Cluster;
+import akka.stream.Materializer;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import de.hpi.ddm.actors.LargeMessageProxy;
 import de.hpi.ddm.actors.Master;
 import de.hpi.ddm.actors.Reaper;
 import de.hpi.ddm.actors.Worker;
@@ -17,57 +15,60 @@ import de.hpi.ddm.configuration.ConfigurationSingleton;
 import scala.concurrent.Await;
 import scala.concurrent.duration.Duration;
 
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class MasterSystem {
-	
-	public static final String MASTER_ROLE = "master";
 
-	public static void start() {
-		final Configuration c = ConfigurationSingleton.get();
-		
-		final Config config = ConfigFactory.parseString(
-				"akka.remote.artery.canonical.hostname = \"" + c.getHost() + "\"\n" +
-				"akka.remote.artery.canonical.port = " + c.getPort() + "\n" +
-				"akka.cluster.roles = [" + MASTER_ROLE + "]\n" +
-				"akka.cluster.seed-nodes = [\"akka://" + c.getActorSystemName() + "@" + c.getHost() + ":" + c.getPort() + "\"]")
-			.withFallback(ConfigFactory.load("application"));
-		
-		final ActorSystem system = ActorSystem.create(c.getActorSystemName(), config);
-		
-		ActorRef reaper = system.actorOf(Reaper.props(), Reaper.DEFAULT_NAME);
-		
-		ActorRef master = system.actorOf(Master.props(), Master.DEFAULT_NAME);
-		
-		Cluster.get(system).registerOnMemberUp(new Runnable() {
-			@Override
-			public void run() {
-				for (int i = 0; i < c.getNumWorkers(); i++)
-					system.actorOf(Worker.props(), Worker.DEFAULT_NAME + i);
-			}
-		});
-		
-		Cluster.get(system).registerOnMemberRemoved(new Runnable() {
-			@Override
-			public void run() {
-				system.terminate();
+    public static final String MASTER_ROLE = "master";
 
-				new Thread() {
-					@Override
-					public void run() {
-						try {
-							Await.ready(system.whenTerminated(), Duration.create(10, TimeUnit.SECONDS));
-						} catch (Exception e) {
-							System.exit(-1);
-						}
-					}
-				}.start();
-			}
-		});
-		
-		System.out.println("Press <enter> to end the application!");
-		try (final Scanner scanner = new Scanner(System.in)) {
-			scanner.nextLine();
-		}
-		
-		system.actorSelection("/user/" + Master.DEFAULT_NAME).tell(new Master.EndMessage(), ActorRef.noSender());
-	}
+    public static void start() {
+        final Configuration c = ConfigurationSingleton.get();
+
+        final Config config = ConfigFactory.parseString(
+                "akka.remote.artery.canonical.hostname = \"" + c.getHost() + "\"\n" +
+                        "akka.remote.artery.canonical.port = " + c.getPort() + "\n" +
+                        "akka.cluster.roles = [" + MASTER_ROLE + "]\n" +
+                        "akka.cluster.seed-nodes = [\"akka://" + c.getActorSystemName() + "@" + c.getHost() + ":" + c.getPort() + "\"]")
+                .withFallback(ConfigFactory.load("application"));
+
+        final ActorSystem system = ActorSystem.create(c.getActorSystemName(), config);
+
+        ActorRef reaper = system.actorOf(Reaper.props(), Reaper.DEFAULT_NAME);
+
+        ActorRef master = system.actorOf(Master.props(), Master.DEFAULT_NAME);
+
+        Cluster.get(system).registerOnMemberUp(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < c.getNumWorkers(); i++)
+                    system.actorOf(Worker.props(), Worker.DEFAULT_NAME + i);
+            }
+        });
+
+        Cluster.get(system).registerOnMemberRemoved(new Runnable() {
+            @Override
+            public void run() {
+                system.terminate();
+
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            Await.ready(system.whenTerminated(), Duration.create(10, TimeUnit.SECONDS));
+                        } catch (Exception e) {
+                            System.exit(-1);
+                        }
+                    }
+                }.start();
+            }
+        });
+
+        System.out.println("Press <enter> to end the application!");
+        try (final Scanner scanner = new Scanner(System.in)) {
+            scanner.nextLine();
+        }
+
+        system.actorSelection("/user/" + Master.DEFAULT_NAME).tell(new Master.EndMessage(), ActorRef.noSender());
+    }
 }
--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,435 +1,172 @@
 package de.hpi.ddm.actors;
 
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import akka.actor.*;
+import akka.NotUsed;
+import akka.actor.AbstractLoggingActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
+import akka.actor.Props;
+import akka.stream.Materializer;
+import akka.stream.SourceRef;
+import akka.stream.StreamRefResolver;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.stream.javadsl.StreamRefs;
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-import de.hpi.ddm.structures.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
 
-	////////////////////////
-	// Actor Construction //
-	////////////////////////
-
-
-	private Map<String, LargeMessage<?>> messages = new HashMap<>();
-
-
-	//private List<BytesMessage<?>> messages = new ArrayList<>();
-
-	public static final String DEFAULT_NAME = "largeMessageProxy";
-	// define how many blocks the message consists of
-	// hardcoded -> bad solution!!
-	public static final short BLOCK_SIZE = 2048; // 2KB
-
-
-
-	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
-	public static String bytesToHex(byte[] bytes) {
-		char[] hexChars = new char[bytes.length * 2];
-		for (int j = 0; j < bytes.length; j++) {
-			int v = bytes[j] & 0xFF;
-			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-		}
-		return new String(hexChars);
-	}
-
-	public static Props props() {
-		return Props.create(LargeMessageProxy.class);
-	}
-
-	////////////////////
-	// Actor Messages //
-	////////////////////
-
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class LargeMessage<T> implements Serializable {
-		private static final long serialVersionUID = 2940665245810221108L;
-		private T message;
-		private ActorRef receiver;
-	}
-
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class BytesMessage<T> implements Serializable {
-		private static final long serialVersionUID = 4057807743872319842L;
-		private T bytes;
-		private ActorRef sender;
-		private ActorRef receiver;
-
-		// added
-		private long uuid;
-		private short id;
-		private short max_id = 0;
-		private short length;
-	}
-
-	/////////////////
-	// Actor State //
-	/////////////////
-
-	/////////////////////
-	// Actor Lifecycle //
-	/////////////////////
-
-	////////////////////
-	// Actor Behavior //
-	////////////////////
-
-	@Override
-	public Receive createReceive() {
-		return receiveBuilder()
-				.match(LargeMessage.class, this::handle)
-				.match(BytesMessage.class, this::handle)
-				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
-				.build();
-	}
-
-	/*
-	public class Wrappper {
-
-		private LargeMessageProxy getOuterClass() {
-			return LargeMessageProxy.this;
-		}
-
-		private void tellRec(LargeMessage<?> msg, ActorSelection receiverProxy, ActorRef receiver) {
-			//ActorSelection receiverProxy = getOuterClass().context().actorSelection(receiver.path().child(DEFAULT_NAME));
-			receiverProxy.tell(new BytesMessage(msg.getMessage(),getOuterClass().sender(),msg.getReceiver()),getOuterClass().self());
-		}
-
-		private CompletionStage<Done> sendStuff(LargeMessage<?> msg, ActorSelection receiverProxy, ActorRef receiver) {
-			return Source.single(msg).runWith(tellRec(msg, receiverProxy, receiver), ActorMaterializer.create(getOuterClass().context()));
-		}
-	}
-
-	private Flow<LargeMessage<?>, BytesMessage<?>, NotUsed> sendMessage(LargeMessage<?> msg) {
-		ActorRef sender = this.getSender();
-		ActorRef receiver = msg.getReceiver();
-		//return
-
-		//return Flow.of(LargeMessage.class).flatMapConcat(msg -> {
-
-		//});
-	}
-
-	private Flow<LargeMessage<?>, BytesMessage<?>, NotUsed> toBytes() {
-		return Flow.of(LargeMessage.class).
-	}
-
-	private Sink<LargeMessage<?>, CompletionStage<Done>> getMsg() {
-		return Flow.of(LargeMessage.class).via(toBytes());
-	}
-
-	private CompletionStage<Done> sendStuff(){
-		private CompletionStage
-	}
-	*/
-
-	/*
-	private Byte[] serializeMessage(LargeMessage<?> msg, Output output){
-
-		Kryo kryo = new Kryo();
-		//kryo.writeClassAndObject(output,msg);
-		//ObjectOutputStream stream = new ObjectOutputStream(output);
-		ByteArrayOutputStream outstream = new ByteArrayOutputStream();
-		Output out = new Output(outstream);
-		kryo.writeClassAndObject(out,msg);
-		outstream.close();
-		out.close();
-		return outstream.toByteArray();
-
-	}
-	*/
-
-	public static byte[] objToByte(LargeMessage<?> msg) throws IOException {
-		//ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-		//ObjectOutputStream objStream = new ObjectOutputStream(byteStream);
-		//objStream.writeObject(msg);
-
-		//return byteStream.toByteArray();
-		try {
-			Kryo k = new Kryo();
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			Output output = new Output(out);
-			k.writeClassAndObject(output, msg);
-			output.close();
-			out.close();
-			return out.toByteArray();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-
-	private void handle(LargeMessage<?> message) {
-		ActorRef receiver = message.getReceiver();
-		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		ObjectOutputStream stream = null; // check if init necessary
-
-		UUID uuid = UUID.randomUUID();
-		long message_id = uuid.getMostSignificantBits();
-
-		try {
-			stream = new ObjectOutputStream(outputStream);
-			stream.writeObject(message);
-			stream.flush();
-
-			// Message
-			byte[] bytes = outputStream.toByteArray();
-			System.out.println(bytesToHex(bytes));
-
-			int byte_length = bytes.length;
-
-			// max id = amount of chunks-1
-			System.out.println("Byte_length: " + byte_length);
-			System.out.println("block_size: " + BLOCK_SIZE);
-			short max_id = (short) Math.ceil(byte_length / BLOCK_SIZE);
-
-			// current block pointer (bytes)
-			int block_pointer = 0;
-			boolean last = false;
-
-			// current block id
-			short block_id = 0;
-
-			while (true) {
-				//stream.flush();
-				BytesMessage<byte[]> block = new BytesMessage<>();
-
-				if (block_pointer + BLOCK_SIZE > byte_length) {
-					block.bytes = Arrays.copyOfRange(bytes, block_pointer, byte_length);
-					// TODO: cast is bad
-					block.length = (short) (byte_length - block_pointer);
-					last = true;
-				} else {
-					block.bytes = Arrays.copyOfRange(bytes, block_pointer, block_pointer + BLOCK_SIZE);
-					block.length = BLOCK_SIZE;
-				}
-				/*
-				block.uuid = message_id;
-				System.out.println("UUID: "+block.uuid);
-				block.sender = this.sender();
-				System.out.println("SENDER: "+block.sender);
-				block.receiver = receiver;
-				System.out.println("RECEIVER: "+block.receiver);
-				block.id = block_id;
-				System.out.println("BLOCKID: "+block.id);
-				block.max_id = max_id;
-				System.out.println("MAX_ID: "+block.max_id);
-				System.out.println("LENGTH: "+block.length);
-				*/
-				// tell receiver
-				receiverProxy.tell(block, this.self());
-				if (last) {
-					break;
-				}
-				// update block_pointer
-				block_pointer = block_pointer + BLOCK_SIZE;
-				block_id++;
-
-			}
-
-		} catch (IOException e) {
-			System.out.println("IOException occured!");
-		}
-
-
-
-		/*
-		System.out.println("NOW TESTING");
-		try {
-			System.out.println(objToByte(message));
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-		*/
-		//message
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
-		//BytesMessage<?> msg = new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver());
-		//System.out.println("TESTOUT: "+msg.bytes);
-
-		// Use Akka Streaming
-		//Source<LargeMessage<?>, NotUsed> src = Source.single(message);
-		//Flow<LargeMessage<?>, BytesMessage<?>, NotUsed> flow = new Flow<LargeMessage<?>, BytesMessage<?>, NotUsed>();
-		//Source.single(message).runWith(getMsg(), ActorMaterializer.create(this.context()));
-		//src.runForeach(i -> receiverProxy.tell(new BytesMessage<>(i.getMessage(), this.sender(),i.getReceiver()),this.self()));
-		//Object
-
-
-		//receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
-	}
-
-
-	Map<Long, Map<Short, byte[]>> map = new HashMap<>();
-	int array_size = 0;
-
-
-	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
-		//Sink<BytesMessage<?>, NotUsed> snk = Sink
-
-		// write uuid to Map
-		if (!map.containsKey(message.uuid)) {
-			map.put(message.uuid, new HashMap<>());
-		}
-
-		// to inner map
-		Map<Short, byte[]> inner_map = map.get(message.uuid);
-		inner_map.put(message.id, (byte[]) message.bytes);
-		array_size = array_size + message.length;
-
-		//for (Map.Entry<Short, byte[]> entry : inner_map.entrySet()) {
-		//	System.out.println(entry.getKey() + ":" + entry.getValue().toString());
-
-		//}
-
-		//System.out.println("Max_ID ist: "+message.max_id);
-
-		List<Short> innermap_as_list = inner_map.keySet().stream().sorted().collect(Collectors.toList());
-
-
-		// print all keys of inner list (all message chunks, belonging to one single message)
-		//innermap_as_list.forEach((i)->System.out.println(i));
-
-
-		// if all message chunks received...
-		// == if received chunk has same id as max_id
-		if (message.max_id == message.id) {
-
-			// all chunks are received
-			/*
-			inner_map.entrySet().forEach(entry->{
-				System.out.println(entry.getKey() + " " + entry.getValue());
-			});
-			*/
-
-
-
-
-			// right now it is required that last received byte is last byte of message
-			// OK - complete_message has correct size
-			byte[] complete_message = new byte[array_size];
-			//System.out.println("TEST_HERE: "+complete_message.length);
-
-			for (Short i : innermap_as_list) {
-				//Short index = innermap_as_list.get(i);
-				// block_size = 2048 -> TODO: hardcoded right now
-				/*
-				System.out.println("########################");
-				System.out.println("DEBUG: "+inner_map.get(index));
-				System.out.println("DEBUG: "+complete_message);
-				System.out.println("DEBUG: "+index*2048);
-				System.out.println("DEBUG: "+inner_map.get(index).length);
-				System.out.println("########################");
-				*/
-				//System.arraycopy();
-				// hier weiter
-				System.arraycopy(inner_map.get(i),0, complete_message,i*BLOCK_SIZE, inner_map.get(i).length);
-			}
-			System.out.println(bytesToHex(complete_message));
-			// view content of complete message
-			//for (int i = 0; i<array_size;i++){
-				//int positive = complete_message[i] & 0xff;
-
-			//}
-
-
-			ByteArrayInputStream bytes_in = new ByteArrayInputStream(complete_message);
-			try {
-				ObjectInputStream object_in = new ObjectInputStream(bytes_in);
-
-				Object o = object_in.readObject();
-
-				Object s = KryoPoolSingleton.get().fromBytes(complete_message);
-
-				System.out.println(s);
-				message.getReceiver().tell(s, message.getSender());
-			} catch (IOException | ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-
-
-
-
-
-
-		// inner_map.keySet().forEach( (value) -> System.out.println(value));
-
-
-
-		/*
-		short max_id = message.max_id;
-
-		ByteArrayInputStream in = new ByteArrayInputStream((byte[]) message.bytes);
-
-		try {
-			ObjectInputStream in_stream = new ObjectInputStream(in);
-			LargeMessage<?> orig = (LargeMessage<?>) in_stream.readObject();
-			//messages
-			messages.put(""+message.uuid+"_"+message.id, orig);
-			message.getReceiver().tell(message.getBytes(), message.getSender());
-
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		//System.out.println(in);
-
-
-
-
-
-		if (max_id == this.messages.size()) {
-			// extrahiere alle schlüssel
-			String[] keys = new String[messages.size()];
-			messages.keySet().toArray(keys);
-
-			// iteriere durch die keys
-			for (int i = 0; i<max_id; i++) {
-				if (keys[i].startsWith(""+message.uuid)) {
-					String id_part = keys[i].split("_")[1];
-					System.out.println(id_part);
-				}
-			}
-
-			//iteriere durch die hashmap
-			//for (int i = 0; i<max_id; i++) {
-
-			//}
-			//baue schlüssel auseinander
-			//behalte nur die mit der uuid von oben
-
-			//sortiere nach der id
-			//baue die messages zusammen
-
-		}
-
-		byte[] msg = (byte[]) message.bytes;
-		System.out.println("HIER SCHAUEN: "+msg);
-	}
-	*/
-
-	}
+    ////////////////////////
+    // Actor Construction //
+    ////////////////////////
+
+    public static final String DEFAULT_NAME = "largeMessageProxy";
+    private static final int MAX_ALLOWED_STREAM_SIZE = 260000;
+    private final StreamRefResolver streamRefResolver = StreamRefResolver.get(this.context().system());
+    private final Materializer materializer = Materializer.apply(this.context().system());
+
+    public static Props props() {
+        return Props.create(LargeMessageProxy.class);
+    }
+
+    ////////////////////
+    // Actor Messages //
+    ////////////////////
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(LargeMessage.class, this::handle)
+                .match(MessageWrapper.class, this::handle)
+                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
+                .build();
+    }
+
+    private void handle(LargeMessage<?> message) {
+        ActorRef receiver = message.getReceiver();
+        ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
+
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+
+            // Call Kryo Serializer
+            Output output = new Output(oos);
+            Kryo kryo = new Kryo();
+            kryo.writeClassAndObject(output, message.getMessage());
+
+            // Need to call .close() before toByteArry to avoid “KryoException: Buffer underflow”
+            output.flush();
+            output.close();
+
+            // message in Bytes
+            byte[] byteArray = bos.toByteArray();
+            this.log().info("Byte Array Length: " + byteArray.length);
+
+            // Create chunks with ids for sort
+            List<Node> noteList = new ArrayList<>();
+            int counter = 1;
+            for (int i = 0; i < byteArray.length; i += (MAX_ALLOWED_STREAM_SIZE + 1)) {
+                int max = i + MAX_ALLOWED_STREAM_SIZE;
+                if (i + MAX_ALLOWED_STREAM_SIZE >= byteArray.length)
+                    max = byteArray.length - 1;
+                noteList.add(new Node(counter++, Arrays.copyOfRange(byteArray, i, max + 1)));
+            }
+
+            // Akka Streams Source -> Graph -> Sink
+            Source<Node, NotUsed> streamLogs = Source.from(noteList);
+            SourceRef<Node> sourceRef = streamLogs.runWith(StreamRefs.sourceRef(), materializer);
+
+            // SourceRef Wrapper
+            receiverProxy.tell(new MessageWrapper(this.streamRefResolver.toSerializationFormat(sourceRef), this.sender(), receiver), this.self());
+        } catch (IOException e) {
+            this.log().error(e, e.getMessage());
+        }
+    }
+
+    /////////////////
+    // Actor State //
+    /////////////////
+
+    /////////////////////
+    // Actor Lifecycle //
+    /////////////////////
+
+    ////////////////////
+    // Actor Behavior //
+    ////////////////////
+
+    private void handle(MessageWrapper message) {
+        ArrayList<Node> nodes = new ArrayList<>();
+        this.streamRefResolver.resolveSourceRef(message.getSourceRef())
+                .getSource()
+                .runWith(Sink.foreach(e -> nodes.add((Node) e)), materializer)
+                .thenAccept((f) -> {
+                    // Sort Nodes for the correct order
+                    nodes.sort(Node::compareTo);
+
+                    // merging all byte arrays
+                    byte[] result = {};
+                    for (Node node : nodes) {
+                        result = ArrayUtils.addAll(result, node.getByteArray());
+                    }
+                    this.log().info("Received Byte Array Length: " + result.length);
+
+                    try (ByteArrayInputStream inputStream = new ByteArrayInputStream(result);
+                         ObjectInputStream ois = new ObjectInputStream(inputStream);
+                         Input input = new Input(ois)) {
+
+                        // deserialize
+                        Kryo kryo = new Kryo();
+                        message.getReceiver().tell(kryo.readClassAndObject(input), message.getSender());
+                        this.log().info("Message send");
+                    } catch (IOException e) {
+                        this.log().error("Stream Error {}", e.getMessage());
+                    } catch (Exception e) {
+                        this.log().error(e, e.getMessage());
+                    }
+                });
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class LargeMessage<T> implements Serializable {
+        private static final long serialVersionUID = 2940665245810221108L;
+        private T message;
+        private ActorRef receiver;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class MessageWrapper implements Serializable {
+        private static final long serialVersionUID = 5707807743872319842L;
+        private String sourceRef;
+        private ActorRef sender;
+        private ActorRef receiver;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Node implements Serializable, Comparable<Node> {
+        private static final long serialVersionUID = 4557807743872319842L;
+        private int key;
+        private byte[] byteArray;
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.key, o.getKey());
+        }
+    }
 }
