a99a65b0ce39f3fc2a151c15a127856d933508d0
Sascha Obst <saschaobst@Saschas-MacBook-Pro.local>
2019-11-14 18:30:33 +0100

    debugging

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 import akka.Done;
 import akka.NotUsed;
@@ -15,6 +16,7 @@ import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Output;
+import de.hpi.ddm.structures.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,12 +30,13 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////////
 
 
-	private Map<String,LargeMessage<?>> messages = new HashMap<>();
+	private Map<String, LargeMessage<?>> messages = new HashMap<>();
+
 
 	//private List<BytesMessage<?>> messages = new ArrayList<>();
 
 	public static final String DEFAULT_NAME = "largeMessageProxy";
-	
+
 	public static Props props() {
 		return Props.create(LargeMessageProxy.class);
 	}
@@ -41,15 +44,19 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Messages //
 	////////////////////
-	
-	@Data @NoArgsConstructor @AllArgsConstructor
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
 	public static class LargeMessage<T> implements Serializable {
 		private static final long serialVersionUID = 2940665245810221108L;
 		private T message;
 		private ActorRef receiver;
 	}
 
-	@Data @NoArgsConstructor @AllArgsConstructor
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
 	public static class BytesMessage<T> implements Serializable {
 		private static final long serialVersionUID = 4057807743872319842L;
 		private T bytes;
@@ -60,12 +67,13 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private long uuid;
 		private short id;
 		private short max_id;
+		private short length;
 	}
-	
+
 	/////////////////
 	// Actor State //
 	/////////////////
-	
+
 	/////////////////////
 	// Actor Lifecycle //
 	/////////////////////
@@ -73,7 +81,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Behavior //
 	////////////////////
-	
+
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
@@ -149,7 +157,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 			Kryo k = new Kryo();
 			ByteArrayOutputStream out = new ByteArrayOutputStream();
 			Output output = new Output(out);
-			k.writeClassAndObject(output,msg);
+			k.writeClassAndObject(output, msg);
 			output.close();
 			out.close();
 			return out.toByteArray();
@@ -171,7 +179,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 
 		// define how many blocks the message consists of
 		// hardcoded -> bad solution!!
-		int block_size = 4096; // 4KB
+		short block_size = 2048; // 2KB
 
 		UUID uuid = UUID.randomUUID();
 		long message_id = uuid.getMostSignificantBits();
@@ -181,51 +189,56 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 			stream.writeObject(message);
 			stream.flush();
 
-
 			// Message
 			byte[] bytes = outputStream.toByteArray();
-			// generate UUID
-
-			//stream.writeObject(message_id);
-			//byte[] bytes_uuid = outputStream.toByteArray();
-			//Actor
-			//stream.writeObject();
 
 			int byte_length = bytes.length;
 
-			short max_id = (short) Math.ceil(byte_length/block_size);
+			// max id = amount of chunks-1
+			System.out.println("Byte_length: " + byte_length);
+			System.out.println("block_size: " + block_size);
+			short max_id = (short) Math.ceil(byte_length / block_size);
 
-			//System.out.println(bytes);
-
+			// current block pointer (bytes)
 			int block_pointer = 0;
 			boolean last = false;
 
+			// current block id
 			short block_id = 0;
 
 			while (true) {
 				BytesMessage<byte[]> block = new BytesMessage<>();
 
-
-				if (block_pointer+block_size>byte_length) {
-					block.bytes = Arrays.copyOfRange(bytes,block_pointer,byte_length);
+				if (block_pointer + block_size > byte_length) {
+					block.bytes = Arrays.copyOfRange(bytes, block_pointer, byte_length);
+					// TODO: cast is bad
+					block.length = (short) (byte_length - block_pointer);
 					last = true;
 				} else {
 					block.bytes = Arrays.copyOfRange(bytes, block_pointer, block_pointer + block_size);
+					block.length = block_size;
 				}
 
 				block.uuid = message_id;
+				System.out.println("UUID: "+block.uuid);
 				block.sender = this.sender();
+				System.out.println("SENDER: "+block.sender);
 				block.receiver = receiver;
+				System.out.println("RECEIVER: "+block.receiver);
 				block.id = block_id;
+				System.out.println("BLOCKID: "+block.id);
 				block.max_id = max_id;
+				System.out.println("MAX_ID: "+block.max_id);
+				System.out.println("LENGTH: "+block.length);
 
 				// tell receiver
-				receiverProxy.tell(block,this.self());
+				receiverProxy.tell(block, this.self());
 				if (last) {
 					break;
 				}
 				// update block_pointer
 				block_pointer = block_pointer + block_size;
+				block_id++;
 
 			}
 
@@ -262,23 +275,89 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		//Object
 
 
-
-
 		//receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
 	}
+
+
+	Map<Long, Map<Short, byte[]>> map = new HashMap<>();
 
 	private void handle(BytesMessage<?> message) {
 		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
 		//Sink<BytesMessage<?>, NotUsed> snk = Sink
-		//
 
+
+		// write uuid to Map
+		if (!map.containsKey(message.uuid)) {
+			map.put(message.uuid, new HashMap<>());
+		}
+
+		// to inner map
+		Map<Short, byte[]> inner_map = map.get(message.uuid);
+		inner_map.put(message.id, (byte[]) message.bytes);
+
+
+		//for (Map.Entry<Short, byte[]> entry : inner_map.entrySet()) {
+		//	System.out.println(entry.getKey() + ":" + entry.getValue().toString());
+
+		//}
+
+		//System.out.println("Max_ID ist: "+message.max_id);
+
+		List<Short> innermap_as_list = inner_map.keySet().stream().sorted().collect(Collectors.toList());
+
+
+		// print all keys of inner list (all message chunks, belonging to one single message)
+		//innermap_as_list.forEach((i)->System.out.println(i));
+
+
+		// if all message chunks received...
+		// == if received chunk has same id as max_id
+		if (message.max_id == message.id) {
+
+			// right now it is required that last received byte is last byte of message
+			byte[] complete_message = new byte[message.max_id+1*2048];
+
+			for (int i=0; i<message.max_id+1; i++) {
+				Short index = innermap_as_list.get(i);
+				// block_size = 2048 -> TODO: hardcoded right now
+				/*
+				System.out.println("########################");
+				System.out.println("DEBUG: "+inner_map.get(index));
+				System.out.println("DEBUG: "+complete_message);
+				System.out.println("DEBUG: "+index*2048);
+				System.out.println("DEBUG: "+inner_map.get(index).length);
+				System.out.println("########################");
+				*/
+				//System.arraycopy();
+				// hier weiter
+				System.arraycopy(inner_map.get(index),0, complete_message,index*2048, inner_map.get(index).length);
+			}
+			ByteArrayInputStream bytes_in = new ByteArrayInputStream(complete_message);
+			try {
+				ObjectInputStream object_in = new ObjectInputStream(bytes_in);
+
+				Object s = KryoPoolSingleton.get().fromBytes(complete_message);
+
+				System.out.println(s);
+				message.getReceiver().tell(s, message.getSender());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+
+
+
+
+		// inner_map.keySet().forEach( (value) -> System.out.println(value));
+
+
+
+		/*
 		short max_id = message.max_id;
 
 		ByteArrayInputStream in = new ByteArrayInputStream((byte[]) message.bytes);
-
-
-
-
 
 		try {
 			ObjectInputStream in_stream = new ObjectInputStream(in);
@@ -293,11 +372,26 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		//System.out.println(in);
 
 
+
+
+
 		if (max_id == this.messages.size()) {
-			//iteriere durch die hashmap
+			// extrahiere alle schlüssel
+			String[] keys = new String[messages.size()];
+			messages.keySet().toArray(keys);
+
+			// iteriere durch die keys
 			for (int i = 0; i<max_id; i++) {
-				messages.
+				if (keys[i].startsWith(""+message.uuid)) {
+					String id_part = keys[i].split("_")[1];
+					System.out.println(id_part);
+				}
 			}
+
+			//iteriere durch die hashmap
+			//for (int i = 0; i<max_id; i++) {
+
+			//}
 			//baue schlüssel auseinander
 			//behalte nur die mit der uuid von oben
 
@@ -308,5 +402,8 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 
 		byte[] msg = (byte[]) message.bytes;
 		System.out.println("HIER SCHAUEN: "+msg);
+	}
+	*/
+
 	}
 }
